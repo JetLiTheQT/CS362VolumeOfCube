2e2063f960cf5eec600b04bb105acf5915126a08
JetLiTheQT <ekkachai.ittihrit2@gmail.com>
2021-05-03 21:46:28 -0700

    Added Fail Tests

--- test_volumeCube.py
+++ test_volumeCube.py
@@ -3,11 +3,20 @@ import volumeCube
 
 class testVolumeCube(unittest.TestCase):
     def test_neg(self):
+        #Passing Test
         self.assertEqual(volumeCube.volume(-1,2,-3), "Invalid Input")
+        #Failing Test
+        #self.assertEqual(volumeCube.volume(-1,2,-3), "-2")
     def test_zero(self):
+        #Passing Test
         self.assertEqual(volumeCube.volume(0,2,0), "Invalid Input")
+        #Failing Test
+        #self.assertEqual(volumeCube.volume(0,2,0), ".67")
     def test_string(self):
+        #Passing Test
         self.assertEqual(volumeCube.volume("a", 1, "asd;kfja"), "Invalid Input")
+        #Failing Test
+        #self.assertEqual(volumeCube.volume("a", 1, "asd;kfja"), "Good Input")
     
 
 
